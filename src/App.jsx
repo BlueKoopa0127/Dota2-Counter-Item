bc746fc3f7c8dc084362816ce66a4f874ff0be2f
BlueKoopa0127 <bluekoopa@me.com>
2022-07-05 15:02:38 +0900

    update write method

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-function App() {
+import { useEffect, useState } from "react";
+import Chart from "./Chart";
+
+export default function App() {
   return (
     <div>
       <h1>Hello, World!</h1>
     </div>
   );
 }
-
-export default App;
