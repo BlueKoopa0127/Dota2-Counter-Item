bf679911f01b61606002d7e308c6e66810477dd8
BlueKoopa0127 <bluekoopa@me.com>
2022-07-05 15:03:31 +0900

    add testcase

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { Test } from "./Model";
+import { ChartTest } from "./Chart";
+import { ModelTest } from "./Model";
 
 //createRoot(document.querySelector("#content")).render(<App />);
-createRoot(document.querySelector("#content")).render(<Test />);
+createRoot(document.querySelector("#content")).render(<ChartTest />);
+//createRoot(document.querySelector("#content")).render(<ModelTest />);
