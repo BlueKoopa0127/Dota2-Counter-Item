d8c57a647a9cd507a34c5732044c87ffd1c0e259
BlueKoopa0127 <bluekoopa@me.com>
2022-07-13 15:09:41 +0900

    change test

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,6 @@ import App from "./App";
 import { ChartTest } from "./Chart";
 import { ModelTest } from "./Model";
 
-//createRoot(document.querySelector("#content")).render(<App />);
-createRoot(document.querySelector("#content")).render(<ChartTest />);
+createRoot(document.querySelector("#content")).render(<App />);
+//createRoot(document.querySelector("#content")).render(<ChartTest />);
 //createRoot(document.querySelector("#content")).render(<ModelTest />);
